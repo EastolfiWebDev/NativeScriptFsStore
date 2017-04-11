f07944dda04b17fdd0f92f016c4d839bcfb393f6
eastolfi <astolfi_wotc@hotmail.com>
2017-04-11 19:41:55 +0000

    feat: Create project structure

---
+++ gulp/tasks/docs.js
@@ -0,0 +1,66 @@
+var gulp = require("gulp");
+var fs = require("fs");
+var gutil = require("gulp-util");
+var rename = require("gulp-rename");
+// var jsdoc = require("gulp-jsdoc3");
+var typedoc = require("gulp-typedoc");
+var jsdoc2md = require("jsdoc-to-markdown");
+var gulpJsdoc2md = require("gulp-jsdoc-to-markdown");
+var conventionalChangelog = require("gulp-conventional-changelog");
+
+gulp.task("doc:api:full", function () {
+    return jsdoc2md.render({ files: "src/**/*.js" })
+        .then(function(output) {
+            return fs.writeFileSync("api/index.md", output);
+        });
+});
+
+gulp.task("doc:api:files", function () {
+    return gulp.src(["src/FileSystemStore.js"])         // CHANGE: Put your documented files
+        .pipe(gulpJsdoc2md(/*{ template: fs.readFileSync("./readme.hbs", "utf8") }*/))
+        .on("error", function (err) {
+            gutil.log(gutil.colors.red("jsdoc2md failed"), err.message);
+        })
+        .pipe(rename(function (path) {
+            path.extname = ".md";
+        }))
+        .pipe(gulp.dest("api"));
+});
+
+gulp.task("doc:app", function (cb) {
+    return gulp.src("./src/FileSystemStore.ts")
+        .pipe(typedoc({
+            // typescript
+            target: "es5",
+    		module: "commonjs",
+    		moduleResolution: "node",
+    		experimentalDecorators: true,
+    		emitDecoratorMetadata: true,
+    		noImplicitAny: false,
+    		suppressImplicitAnyIndexErrors: true,
+    		
+    		// typedoc
+    		out: "doc",
+    		json: "doc/out.json",
+    		
+    		name: "FileSystemStore",
+    		ignoreCompilerErrors: false,
+    		version: true
+        }));
+});
+
+gulp.task("changelog", function () {
+    return gulp.src("CHANGELOG.md", {
+        buffer: false
+    })
+    .pipe(conventionalChangelog({
+        preset: "angular",
+        outputUnreleased: true,
+        releaseCount: 0
+    }, {    // CHANGE: Put your github repository info
+        host: "https://github.com",
+        owner: "eastolfi",
+        repository: "FileSystemStore"
+    }))
+    .pipe(gulp.dest("./"));
+});
